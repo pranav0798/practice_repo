1b8c7c0c9a8fac52ce726d2c2abd1cbacd48a314
Stepin-105340 <69068098+pranav0798@users.noreply.github.com>
2020-12-13 19:58:18 +0530

    Update CLASSES.py

--- CLASSES.py
+++ CLASSES.py
@@ -1,24 +1,64 @@
+import re
+import json
+import math
+
+# class declared
+
 class currency:
     value = 0
+
+#constructor passed
 
     def __init__(self, value):
         self.value = value
 
+#objects initialized
+
+    def USD(self):
+        result = 1 * self.value
+        return result
 
     def USD_TO_INR(self):
-        result = float(73.66 * self.value)
+        result = float(1 * self.value / 73.66) 
+        return result
+    
+    def USD_TO_GBP(self):
+        result = float(0.85 * self.value)
+        return result
+    
+    def USD_TO_AUD(self):
+        result = float(1 * self.value / 1.33)
+        return result
+    
+    def USD_TO_JPY(self):
+        result = float(1 * self.value / 112.63)
         return result
 
-    def INR_TO_USD(self):
-        result = float((self.value * 1) / 73.66)
-        return result
+#class derived (inheritance)        
 
+class notation(currency):
+    pass
+
+#input entered
 
 input_value = float(input("Input currency in USD: "))
-USD = currency(input_value)
-print(USD.USD_TO_INR(), "RUPEES")
+USD1 = currency(input_value)
+print(USD1.USD(),"US DOLLARS")
 
 input_value = float(input("Input currency in INR: "))
 INR = currency(input_value)
-print(INR.INR_TO_USD(), "US DOLLARS")
+print(INR.USD_TO_INR(),"US DOLLARS")
 
+input_value = float(input("Input currency in GBP: "))
+GBP = currency(input_value)
+print(GBP.USD_TO_GBP(),"US DOLLARS")
+
+input_value = float(input("Input currency in AUD: "))
+AUD = currency(input_value)
+print(AUD.USD_TO_AUD(),"US DOLLARS")
+
+input_value = float(input("Input currency in JPY: "))
+JPY = currency(input_value)
+print(JPY.USD_TO_JPY(),"US DOLLARS")
+
+print(re.match("[A-Z a-z 0-9]+@+[a-z]+.+[a-z]","Pranavbhardhwaj98@gmail.com"))
